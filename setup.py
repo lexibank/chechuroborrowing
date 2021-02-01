f1f1f8cb4dbc143951c23fb510ab9978800746a3
Christoph Rzymski <christoph@functor-argument.net>
2021-02-01 13:01:43 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -7,29 +7,28 @@ with open("metadata.json", encoding="utf-8") as fp:
 
 
 setup(
-    name='lexibank_chechuroborrowing',
+    name="lexibank_chechuroborrowing",
     version="1.0",
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_chechuroborrowing'],
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_chechuroborrowing"],
     include_package_data=True,
     zip_safe=False,
     entry_points={
-        'lexibank.dataset': [
-            'chechuroborrowing=lexibank_chechuroborrowing:Dataset',
+        "lexibank.dataset": [
+            "chechuroborrowing=lexibank_chechuroborrowing:Dataset",
         ],
-        'cldfbench.commands': [
-            'chechuroborrowing=commands',
-        ]
+        "cldfbench.commands": [
+            "chechuroborrowing=commands",
+        ],
     },
     install_requires=[
-        'pylexibank>=1.1.1',
-        'beautifulsoup4>=4.7.1',
+        "pylexibank>=2.1",
     ],
     extras_require={
-        'test': [
-            'pytest-cldf',
+        "test": [
+            "pytest-cldf",
         ],
     },
 )
